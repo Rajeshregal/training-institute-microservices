618f8a65b022842775848c84e1e7580a1c8b2328
rajesh <alwaysrajesh946@gmail.com>
2026-04-27 01:14:31 +0530

    docker chnages

--- student-service/src/main/java/com/institute/student/StudentServiceApplication.java
+++ student-service/src/main/java/com/institute/student/StudentServiceApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+
+@SpringBootApplicationcd
 @PropertySource(value = "classpath:StudentApplicationConfig.yaml", factory = com.institute.student.config.YamlPropertySourceFactory.class)
 public class StudentServiceApplication {
 
