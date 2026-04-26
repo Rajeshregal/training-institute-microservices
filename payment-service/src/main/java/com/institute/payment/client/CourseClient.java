110124ba1fb3bea0a121971bdb8a13a754b5f8ab
rajesh <alwaysrajesh946@gmail.com>
2026-04-26 23:52:02 +0530

    till bootstrap implementataion

--- payment-service/src/main/java/com/institute/payment/client/CourseClient.java
+++ payment-service/src/main/java/com/institute/payment/client/CourseClient.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "course-service",
-        url = "http://localhost:8084"
+        name = "course-service"
+     //   url = "http://localhost:8084"
 )
 public interface CourseClient {
 
--- payment-service/src/main/java/com/institute/payment/client/StudentClient.java
+++ payment-service/src/main/java/com/institute/payment/client/StudentClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name="student-service",
-        url="http://localhost:8082"
+        name="student-service"
+     //   url="http://localhost:8082"
 )
 public interface StudentClient {
     @GetMapping("/api/student/code/{studentCode}")
